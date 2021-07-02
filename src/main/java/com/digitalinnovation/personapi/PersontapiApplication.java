71ea94fbccb1824b23681c0277053f9493d2c716
lgomes-lgomes <leogomes2002@gmail.com>
2021-07-02 18:49:51 -0300

    first commit

---
+++ src/main/java/com/digitalinnovation/personapi/PersontapiApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.personapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersontapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersontapiApplication.class, args);
+	}
+
+}
